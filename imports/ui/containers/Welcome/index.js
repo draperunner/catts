ec1bd97361511e23362eef5bbc99eeedfdf88405
Mats Byrkjeland <matsbyr@gmail.com>
2017-01-30 14:53:46 +0100

    Add a description on the Welcome page

--- imports/ui/containers/Welcome/index.js
+++ imports/ui/containers/Welcome/index.js
@@ -8,7 +8,19 @@ const Welcome = () => (
   <div className="row">
     <div className="col-xs-12">
       <h1>Welcome!</h1>
-      <p>Annotating tweets is a cool thing to do. Are you ready?</p>
+      <p>
+        CATTS is an abbrevation for Crowdsourced Annotation Tool for Twitter Sentiment. Its goal is to generate
+        a manually annotated dataset for use in Twitter classification tasks, like sentiment or sarcasm classification.
+        By using CATTS, you contribute to research in the fields of Natural Language Processing and Artificial
+        Intelligence.
+      </p>
+      <p>
+        As tweets are annotated, the data set is instantly updated and available for download on
+        the <Link to="/download">Download</Link> page. Both text and JSON formats are available.
+      </p>
+      <p>
+        Please see the <Link to="/guidelines">Guidelines</Link> page for instructions on how to annotate the tweets.
+      </p>
       <Link to="/annotate"><RaisedButton primary label="Start Annotation" /></Link>
     </div>
   </div>
